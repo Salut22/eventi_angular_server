e0eda49c5fc266a35821e74e50eb9868b2363f93
claudio.saluti <claudio.saluti@studenti.unicam.it>
2016-06-23 17:41:59 +0200

    rimozione scrape

--- scrape/scrape_scripts/virgilio/progweb - Abbruzzo.bat
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Questo script legge una provincia dall'array di tutte le province di italia
-Setlocal EnableDelayedExpansion
-set province=teramo pescara chieti l-aquila
-#(for %%a in (%province%) do (
-#   echo %%a
-#))
-casperjs virgilio.js %province%
